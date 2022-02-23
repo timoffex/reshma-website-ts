3d575daca90fa02122e6943762e7c2508115527e
Timofey Peshin <timoffex@gmail.com>
2022-02-22 19:54:03 -0800

    Better animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
+import { RouterModule, UrlMatchResult, UrlSegment } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { UnwrapDirective } from './unwrap.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    UnwrapDirective,
   ],
   imports: [
     BrowserAnimationsModule,
     RouterModule.forRoot([
       { path: '', pathMatch: 'full', redirectTo: '/home' },
-      { path: 'home', component: HomeComponent },
-      { path: 'detail/:detailid', component: HomeComponent }
+      {
+        component: HomeComponent,
+        // Hack-around to avoid reinitializing the component when changing from /home to /home/123
+        // (needed for animations)
+        matcher: (segments: UrlSegment[]): UrlMatchResult | null => {
+          if (segments[0].path === 'home') {
+            if (segments.length === 1) {
+              return {
+                consumed: segments,
+                posParams: {},
+              };
+            } else if (segments.length === 2) {
+              return {
+                consumed: segments,
+                posParams: { detailid: segments[1] }
+              };
+            }
+          }
+
+          return null;
+        }
+      },
     ])
   ],
   providers: [],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, animateChild, group, query, stagger, state, style, transition, trigger } from '@angular/animations';
 import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { map, Observable } from 'rxjs';
+import { ActivatedRoute, Router, RouteReuseStrategy } from '@angular/router';
+import { BehaviorSubject, map, Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -9,8 +9,54 @@ import { map, Observable } from 'rxjs';
   styleUrls: ['./home.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   animations: [
-    trigger('flipToDetail', [
-    ])
+    trigger('homePageState', [
+      transition('Grid => Detail', [
+        query('@detail', style({ visibility: 'hidden' })),
+        query('@grid', animateChild()),
+      ]),
+      transition('Detail => Grid', [
+        query('@grid', style({ visibility: 'hidden' })),
+        query('@detail', animateChild()),
+        query('@grid', [
+          style({ visibility: 'visible' }),
+          animateChild(),
+        ])
+      ]),
+    ]),
+    trigger('detail', [
+      transition(':leave', [
+        query('img', [
+          animate('500ms ease-in', style({
+            transform: 'translateY(-100%)',
+            opacity: 0,
+          }))
+        ])
+      ])
+    ]),
+    trigger('grid', [
+      transition(':enter', [
+        query('.item', [
+          style({ opacity: 0, transform: 'translateY(50%)' }),
+          stagger(100, [
+            animate('300ms')
+          ]),
+        ])
+      ]),
+      transition(':leave', [
+        query('.item', [
+          stagger(50,
+            animate('1s ease-in', style({
+              transform: 'rotateY(180deg)'
+            }))
+          ),
+        ]),
+        group([
+          query('.item--front, .item--back', [
+            animate('700ms ease-in', style({ borderRadius: 0 })),
+          ]),
+        ]),
+      ]),
+    ]),
   ]
 })
 export class HomeComponent implements OnInit {
@@ -18,20 +64,31 @@ export class HomeComponent implements OnInit {
     private readonly activatedRoute: ActivatedRoute) { }
 
   items = Array.from(Array(10), (_, i) => `item_${i}`);
-
   selectedItem$: Observable<string> | undefined;
+  canClick: boolean = true;
 
   ngOnInit(): void {
     this.selectedItem$ = this.activatedRoute.params.pipe(map((params) => params['detailid']));
   }
 
   selectItemAt(row: number, col: number): void {
-    // this.router.navigate(['detail', item], {
-    //   state: { 'image_bounds': 'xyz' }
-    // });
+    if (!this.canClick) return;
+    this.router.navigate(['home', 'xyz']);
   }
 
-  flipData(row: number, col: number): void {
+  onGridAnimationStart(): void {
+    this.canClick = false;
+  }
 
+  onGridAnimationEnd(): void {
+    this.canClick = true;
+  }
+
+  goHome(): void {
+    this.router.navigate(['home']);
+  }
+
+  homePageState(selectedItem: string): string {
+    return selectedItem ? 'Detail' : 'Grid';
   }
 }
